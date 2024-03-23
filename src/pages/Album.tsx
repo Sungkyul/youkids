3998458378fb804f3341fff9097e5e4d16d8098a
정현수 <trevor0910@naver.com>
2024-03-24 01:31:19 +0900

    feat: 앨범 생성 페이지 1차

---
+++ src/pages/Album.tsx
@@ -0,0 +1,326 @@
+import React from "react";
+import image1 from "../assets/image1.svg";
+import image2 from "../assets/image2.svg";
+import image3 from "../assets/image3.svg";
+import image4 from "../assets/image4.svg";
+import image5 from "../assets/image5.svg";
+import image6 from "../assets/image6.svg";
+import Plus from "../assets/Plus.svg";
+
+function Album() {
+  return (
+    <div className="w-full mx-auto">
+      <div className="justify-center py-4">
+        <p className=" text-[20px] text-center font-bold">앨범 생성</p>
+      </div>
+
+      <div className="border-b border-gray-300">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="border-b border-gray-300">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="border-b border-gray-300">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="border-b border-gray-300">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="border-b border-gray-300">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <div className="flex parent justify-between pt-2">
+          <div className="parent pl-4">
+            <p className="text-left font-bold ">운동회</p>
+          </div>
+          <div className="parent pr-4 pt-1">
+            <p className="text-right text-xs">23.05.16</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between pb-2">
+          <div className="flex pl-4">
+            <img
+              src={image1}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image2}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image3}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image4}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image5}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+            <img
+              src={image6}
+              alt="짱구"
+              className="w-[35px] h-[35px] pr-[7px]"
+            />
+          </div>
+
+          <div className="pr-6 pt-1.5">
+            <img src={Plus} alt="앨범생성" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="justify-center py-4">
+        <p className="text-xs text-center text-gray-600">
+          최근 30일 안에 전송 받은 사진만 볼 수 있습니다.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Album;
