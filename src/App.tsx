9244fb8f763ce4cade612349bd29fce1bd9c64d0
walter230 <ansemr99@gmail.com>
2024-04-19 14:58:18 +0900

    로그인/가입 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Album from "./pages/Album";
+import Join_ID from "./pages/Join_ID";
+import Join_PW from "./pages/Join_PW";
+import Join_PWO from "./pages/Join_PWO";
+import Join_Name from "./pages/Join_Name";
+import Join_Profile from "./pages/Join_Profile";
+import Join_Face from "./pages/Join_Face";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +18,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/Album" element={<Album />} />
+          <Route path="/Join_ID" element={<Join_ID />} />
+          <Route path="/Join_PW" element={<Join_PW />} />
+          <Route path="/Join_PWO" element={<Join_PWO />} />
+          <Route path="/Join_Name" element={<Join_Name />} />
+          <Route path="/Join_Profile" element={<Join_Profile />} />
+          <Route path="/Join_Face" element={<Join_Face />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </Router>
     </div>
